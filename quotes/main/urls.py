4433d2dcb6915467c51921ebe1d6acf957ef36bf
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-04-12 14:07:38 +0300

    main

--- quotes/main/urls.py
+++ quotes/main/urls.py
@@ -3,9 +3,9 @@ from . import views
 from .LogTemplateView import LogTemplateView
 
 urlpatterns = [
-    path('', views.index, name="main"),
+    path('test', views.index, name="main"),
     # path('login', views.logForm, name="logform"),
     path('projects/<str:pk>', views.projects, name="projects"),
-    path('logintest', LogTemplateView.as_view(), name="testform"),
+    path('', LogTemplateView.as_view(), name="testform"),
     # path('test', views.test, name="test"),
 ]
